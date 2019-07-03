4758800f620082f6ea2ac55f9482482bc1ba5386
arnobl <1052622+arnobl@users.noreply.github.com>
2019-07-03 21:28:42 +0200

    fix

--- analyse_kconfig_help_msg.py
+++ analyse_kconfig_help_msg.py
@@ -1,56 +1,46 @@
 #!/usr/bin/python3
 
-# call: python3 analyse_kconfig_help_msg.py 2>/dev/null to ignore warning messages
-# you should put this script in the source folder of a kernel.
-# You may also edit the inDIR (source path that contains the Kconfig files).
+# documentation to use kconfig: https://github.com/TuxML/ProjetIrma/tree/dev/miscellaneous/special-config/Kconfiglib
+# make [ARCH=arch] scriptconfig SCRIPT=analyse_kconfig_help_msg.py
 
 import sys
-from kconfiglib import Kconfig, Symbol, Choice, MENU, COMMENT, KconfigError
-import os, fnmatch
+from kconfiglib import Kconfig, Symbol
 import csv
 
-
-# the kernel location
-inDIR = '.'
-# the kconfig file pattern
-pattern = 'Kconfig*'
-# the list of gathered kconfig files
-fileList = []
+#the kconfig file produced by make and provided as argument
+kconf = Kconfig(sys.argv[1])
 # the help dict: keys are config names, values are help messages (when exist)
 helps = {}
-
-# Gathering all the kconfig files of the kernel
-for dName, sdName, fList in os.walk(inDIR):
-    for fileName in fList:
-        # If is a Kconfig file
-        if fnmatch.fnmatch(fileName, pattern):
-            fileList.append(os.path.join(dName, fileName))
-
+# counting options not documented
+undoc = 0
+# The number of options
+optionsCount = 0
 
 def analyse_help(node):
-    while node:
-        if isinstance(node.item, Symbol) and str(node.help) != "None":
-            helpMsg = str(node.help)
-            helps[node.item.name] = helpMsg.replace("\n"," ").replace(";", ".").replace('"', "")
+    global undoc
+    global helps
+    global optionsCount
 
-#            if "size" in helpMsg or "reduce" in helpMsg:
-#                print(helpMsg)
+    while node:
+        if isinstance(node.item, Symbol):
+            optionsCount = optionsCount + 1
+            if str(node.help) == "None":
+                undoc = undoc + 1
+            else:
+                helpMsg = str(node.help)
+                helps[node.item.name] = helpMsg.replace("\n"," ").replace(";", ".").replace('"', "")
 
         if node.list:
             analyse_help(node.list)
 
         node = node.next
 
-# Browsing the kconf files to analyse them
-for kfile in fileList:
-    try:
-        kconf = Kconfig(kfile)
-        analyse_help(kconf.top_node)
-    except KconfigError:
-        print("==>>> KconfigError for file %s"%(kfile))
+analyse_help(kconf.top_node)
 
 # Saving the dic as an CSV file
 with open('helpMsgs.csv', 'w') as file:
     for key in helps.keys():
         file.write("%s;%s\n"%(key,helps[key]))
 
+print("Options: %d"%optionsCount)
+print("Options not documented: %d"%undoc)
