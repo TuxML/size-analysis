aff7be7ca45c8e85023a2063d1ad225b176bfa03
Mathieu Acher <mathieu.acher@irisa.fr>
2019-07-16 17:21:19 +0200

    support for 4.15

--- tuxml.py
+++ tuxml.py
@@ -7,3 +7,11 @@ dataset_size_filename = os.path.join(dirname, dataset_size_location)
 
 def load_dataset(): 
     return pd.read_pickle(dataset_size_filename)
+
+
+dataset415_size_location = "../tuxml-size-analysis-datasets/all_size_withyes-415.pkl"
+dataset415_size_filename = os.path.join(dirname, dataset415_size_location)
+
+## load dataset for Linux 4.15
+def load_dataset_415(): 
+    return pd.read_pickle(dataset415_size_filename)
