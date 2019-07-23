e4c693a686663a726081c7b71039d91aa616d438
arnobl <1052622+arnobl@users.noreply.github.com>
2019-07-23 15:12:52 +0200

    up

--- compareSizeResults.py
+++ compareSizeResults.py
@@ -47,24 +47,36 @@ def produceOptionDocURL(option):
 # write the results of the analysis in a file
 # options: the list of options to write
 # filename: the filename to use
-def writeResultsInFile(options, filename, withDoc):
+# withDoc: whether the kernel doc must be included
+# computeRankingOption: a lambda to execute. Format: opt => None or a string
+def writeResultsInFile(options, filename, withDoc, computeRankingOption):
     global helps
     with open(filename, 'w') as file:
         for opt in options:
-            if withDoc and opt in helps:
-                file.write("%s,%s,%s\n"%(opt, produceOptionDocURL(opt), '"' + helps[opt] + '"'))
-            else:
-                file.write("%s,%s\n"%(opt, produceOptionDocURL(opt)))
+            doc = helps[opt] if withDoc and opt in helps else ""
+            value = computeRankingOption(opt) if computeRankingOption != None and computeRankingOption(opt) != None else ""
+
+            file.write("%s,%s,%s,%s\n"%(opt, produceOptionDocURL(opt), value, '"' + doc + '"'))
 
 # Reading the options manually spotted
 manualOptionsFile = open("optionsRelatedToSize.txt","r")
 manualOptionsLines = manualOptionsFile.read().splitlines()
 manualOptionsFile.close()
 
-# Reading the options automatically spotted
-autoOptionsLists = pd.read_csv("feature_importanceRF.csv", nrows=nbRowsToAnalyse, usecols=[0]).values.tolist() # index_col=0, sep=',', dtype={0: str}, squeeze=True)
+# Reading the `nbRowsToAnalyse` first options automatically spotted (options name only)
+autoOptionsLists = pd.read_csv("feature_importanceRF.csv", nrows=nbRowsToAnalyse, usecols=[0]).values.tolist()
 # flatting the list of lists
 autoOptions = list(itertools.chain.from_iterable(autoOptionsLists))
+
+# Reading ALL the options automatically spotted (options name + importance value)
+# autoOptionsDict = pd.read_csv("feature_importanceRF.csv", sep=',', header=None, index_col=0, names=["option", "importance"])['importance'].to_dict()
+autoOptionsDataFrame = pd.read_csv("feature_importanceRF.csv", sep=',', header=None, names=["option", "importance", "ranking"], index_col=0)
+# creating a new column with the ranking of the option
+idx = [i for i in range(1, len(autoOptionsDataFrame) + 1)]
+# Adding the new column in the data frame
+autoOptionsDataFrame['ranking'] = idx
+# transforming the data frame into a dict
+autoOptionsDict = autoOptionsDataFrame.to_dict('index')
 
 # Computing the options spotted by both
 optionsInBoth = list(filter(lambda opt: opt in autoOptions, manualOptionsLines))
@@ -72,16 +84,19 @@ optionsInBoth = list(filter(lambda opt: opt in autoOptions, manualOptionsLines))
 optionsInManualOnly = list(filter(lambda opt: not (opt in autoOptions), manualOptionsLines))
 # Computing the options not present in the optionsRelatedToSize list
 optionsInAutoOnly = list(filter(lambda opt: not (opt in manualOptionsLines), autoOptions))
+# Computing the options not present in the feature_importance list but present in the kernel
+optionsKernelNotInAuto = list(filter(lambda opt: not (opt in autoOptions), helps.keys()))
 
 # Extracting the documentation from the kernel
 getDocumentation(kconf.top_node)
 
-# Computing the options of feature_importance, not present in optionsRelatedToSize, that do not have documentation
+# Computing the options of `feature_importance`, not present in `optionsRelatedToSize`, that do not have documentation
 optionsInAutoOnlyWithoutDoc = list(filter(lambda opt: opt in helps and len(helps[opt]) == 0, optionsInAutoOnly))
-# Computing the options of feature_importance, not present in optionsRelatedToSize, that have documentation
+# Computing the options of `feature_importance`, not present in `optionsRelatedToSize`, that have documentation
 optionsInAutoOnlyWithDoc = list(filter(lambda opt: opt in helps and len(helps[opt]) > 0, optionsInAutoOnly))
 
 # Writing the results
-writeResultsInFile(optionsInBoth, 'optionsInBoth.csv', False)
-writeResultsInFile(optionsInAutoOnlyWithoutDoc, 'optionsInAutoOnlyWithoutDoc.csv', False)
-writeResultsInFile(optionsInAutoOnlyWithDoc, 'optionsInAutoOnlyWithDoc.csv', True)
+writeResultsInFile(optionsInBoth, 'optionsInBoth.csv', False, lambda opt: str(autoOptionsDict[opt]['ranking']) if opt in autoOptionsDict else None)
+writeResultsInFile(optionsInAutoOnlyWithoutDoc, 'optionsInAutoOnlyWithoutDoc.csv', False, lambda opt: None)
+writeResultsInFile(optionsInAutoOnlyWithDoc, 'optionsInAutoOnlyWithDoc.csv', True, lambda opt: None)
+writeResultsInFile(optionsKernelNotInAuto, 'optionsKernelNotInAuto.csv', True, lambda opt: None)
